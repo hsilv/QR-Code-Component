ff8dfdb7fd94b2e0750a8cd29b4b909a7f1f5aea
hsilv <hm.sebastiansilva@gmail.com>
2023-10-09 16:45:47 -0600

    themeSelector

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,40 @@ import { useContext } from "react";
 import "./App.module.scss";
 import ThemeContext from "./context/ThemeContext";
 import { availableThemes } from "./data/themes";
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
+import ThemeIcon from "./assets/theme";
 
 function App() {
-
-  const {theme, setTheme} = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
   const changeTheme = (event) => {
     const input = parseInt(event.target.value);
-    for(const idTheme in availableThemes){
-      if(input === parseInt(idTheme)){
+    for (const idTheme in availableThemes) {
+      if (input === parseInt(idTheme)) {
         setTheme(availableThemes[idTheme]);
         break;
       }
     }
-  }
+  };
 
   return (
-  <div className={styles.page} theme={theme}>
-    Color
-    <input className={styles.slider} type="range" min={1} max={3} onChange={changeTheme} defaultValue={1}/>
-  </div>
+    <div className={styles.page} theme={theme}>
+      <header className={styles.header}>
+        <h1>QR Code Generator</h1>
+        <div className={styles.themeControl}>
+          <span>{theme}</span>
+          <ThemeIcon className={styles.ThemeIcon}/>
+          <input
+            className={styles.slider}
+            type="range"
+            min={1}
+            max={3}
+            onChange={changeTheme}
+            defaultValue={1}
+          />
+        </div>
+      </header>
+    </div>
   );
 }
 
